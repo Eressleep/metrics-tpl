43d048d817c0ebc11e8482ae823384ae6eccfddf
Alex <eresken@icloud.com>
2026-03-26 01:59:55 +0300

    iter13 | del check

--- internal/storage/db_storage.go
+++ internal/storage/db_storage.go
@@ -285,14 +285,6 @@ func (s *DBStorage) GetAllGauges() map[string]float64 {
 		result[name] = value
 	}
 
-	// Проверяем ошибки и явно используем переменную
-	rowsErr := rows.Err()
-	if rowsErr != nil {
-		s.logger.Error("Error iterating gauges", zap.Error(rowsErr))
-		// Явно используем переменную, чтобы линтер не ругался
-		_ = rowsErr
-	}
-
 	return result
 }
 
@@ -329,14 +321,6 @@ func (s *DBStorage) GetAllCounters() map[string]int64 {
 			continue
 		}
 		result[name] = value
-	}
-
-	// Проверяем ошибки и явно используем переменную
-	rowsErr := rows.Err()
-	if rowsErr != nil {
-		s.logger.Error("Error iterating counters", zap.Error(rowsErr))
-		// Явно используем переменную, чтобы линтер не ругался
-		_ = rowsErr
 	}
 
 	return result
