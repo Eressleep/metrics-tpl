38ab00907856654ba21bf353caa56c7578fe9da3
Semenov Alexander Sergeevich <sasemenov@sberdevices.ru>
2026-06-13 00:31:00 +0300

    iter20 | fix

--- internal/migrator/migrator.go
+++ internal/migrator/migrator.go
@@ -2,54 +2,35 @@
 package migrator
 
 import (
-	"context"
 	"database/sql"
-	"embed"
 	"fmt"
 
-	"github.com/Eressleep/metrics-tpl/pkg/retry"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
-	"github.com/golang-migrate/migrate/v4/source/iofs"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"go.uber.org/zap"
 )
 
-//go:embed migrations/*.sql
-var migrationsFS embed.FS
-
+// Migrator выполняет миграции базы данных
 type Migrator struct {
 	logger *zap.Logger
 }
 
+// NewMigrator создает новый экземпляр Migrator
 func NewMigrator(logger *zap.Logger) *Migrator {
 	return &Migrator{
 		logger: logger,
 	}
 }
 
+// Up запускает миграции
 func (m *Migrator) Up(dsn string) error {
 	m.logger.Info("Starting database migrations")
 
-	var db *sql.DB
-
-	err := retry.Do(context.TODO(), func() error {
-		var err error
-		db, err = sql.Open("postgres", dsn)
-		if err != nil {
-			return err
-		}
-
-		if err := db.Ping(); err != nil {
-			db.Close()
-			return err
-		}
-
-		return nil
-	}, nil)
-
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
-		return fmt.Errorf("failed to connect to database: %w", err)
+		return fmt.Errorf("failed to open database: %w", err)
 	}
 	defer db.Close()
 
@@ -58,12 +39,11 @@ func (m *Migrator) Up(dsn string) error {
 		return fmt.Errorf("failed to create postgres driver: %w", err)
 	}
 
-	source, err := iofs.New(migrationsFS, "migrations")
-	if err != nil {
-		return fmt.Errorf("failed to create migration source: %w", err)
-	}
-
-	migrator, err := migrate.NewWithInstance("iofs", source, "postgres", driver)
+	migrator, err := migrate.NewWithDatabaseInstance(
+		"file://internal/migrator/migrations",
+		"postgres",
+		driver,
+	)
 	if err != nil {
 		return fmt.Errorf("failed to create migrator: %w", err)
 	}
